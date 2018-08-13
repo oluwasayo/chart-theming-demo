7dc3a8b0e5e53f63f23b4d477f8ec60917f4b93b
Sayo Oladeji <sayo@vaadin.com>
2018-08-13 13:57:55 +0300

    Charts 6 dynamic colors demo

--- src/main/java/com/vaadin/starter/skeleton/MainView.java
+++ src/main/java/com/vaadin/starter/skeleton/MainView.java
@@ -3,51 +3,58 @@ package com.vaadin.starter.skeleton;
 import com.vaadin.flow.component.charts.Chart;
 import com.vaadin.flow.component.charts.model.Configuration;
 import com.vaadin.flow.component.charts.model.ListSeries;
+import com.vaadin.flow.component.charts.model.Tooltip;
 import com.vaadin.flow.component.charts.model.XAxis;
 import com.vaadin.flow.component.charts.model.YAxis;
+import com.vaadin.flow.component.charts.model.style.SolidColor;
 import com.vaadin.flow.component.dependency.HtmlImport;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.page.BodySize;
 import com.vaadin.flow.router.Route;
 
-/**
- * The main view contains a button and a template element.
- */
-@HtmlImport("styles/shared-styles.html")
+import java.util.Random;
+import java.util.stream.IntStream;
+
 @Route("")
+@BodySize(height = "100vh", width = "100vw")
+@HtmlImport("styles/shared-styles.html")
 public class MainView extends VerticalLayout {
 
+    private Random random = new Random();
+
     public MainView() {
-        ThinContainer thinContainer = new ThinContainer();
-        CustomContainer customContainer1 = new CustomContainer();
-        customContainer1.getElement().appendChild(createNewChart().getElement());
-        thinContainer.getElement().appendChild(customContainer1.getElement());
+        Chart chart = createNewChart();
+        chart.addChartClickListener(e -> {
+            // Select a random series and set it to a random color
+            // by setting a CSS variable and let the browser do the coloring
+            int seriesIndex = random.nextInt(5);
 
-        ThickContainer thickContainer = new ThickContainer();
-        CustomContainer customContainer2 = new CustomContainer();
-        customContainer2.getElement().appendChild(createNewChart().getElement());
-        thickContainer.getElement().appendChild(customContainer2.getElement());
+            getElement().getStyle().set("--vaadin-charts-color-" + seriesIndex, randomColor());
+        });
 
-        add(thinContainer, thickContainer);
+        add(chart);
     }
 
     private Chart createNewChart() {
         Chart chart = new Chart();
 
         Configuration configuration = chart.getConfiguration();
-        configuration.setTitle("Example Column Chart");
+        configuration.setTitle("Click on the Chart as much as you like :)");
 
-        configuration.addSeries(new ListSeries("Tokyo", 20, 12, 34, 23, 65, 8, 4, 7, 76, 19, 20, 8));
+        IntStream.range(0, 5).forEach(e -> configuration.addSeries(new ListSeries(randomValues())));
 
-        XAxis x = new XAxis();
-        x.setCategories("Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug",
-                "Sep", "Oct", "Nov", "Dec");
-        configuration.addxAxis(x);
-
-        YAxis y = new YAxis();
-        y.setMin(0);
-        y.setTitle("Rainfall (mm)");
-        configuration.addyAxis(y);
+        Tooltip tooltip = new Tooltip();
+        tooltip.setEnabled(false);
+        configuration.setTooltip(tooltip);
 
         return chart;
+    }
+
+    private Number[] randomValues() {
+        return IntStream.generate(() -> 1 + random.nextInt(10)).boxed().limit(10).toArray(Number[]::new);
+    }
+
+    private String randomColor() {
+        return new SolidColor(random.nextInt(256), random.nextInt(256), random.nextInt(256)).toString();
     }
 }
